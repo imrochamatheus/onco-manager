3cd126db24f772acb9e63da6441dcaa3bed845ea
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-12 08:34:31 -0300

    refactor: Removido AppDataSource

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.listen(process.env.PORT || 3000, () =>
+  console.log("server listening on port " + (process.env.PORT || 3000))
+);
+
 export default app;
