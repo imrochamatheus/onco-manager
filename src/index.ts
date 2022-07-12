12e35d76a82fb651160efbd6f9c0f22bff1d1e08
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-12 09:20:47 -0300

    feat: implementado middleware para tratamento global de erros

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import router from "./routes";
+import "express-async-errors";
+import handleAppErrorMiddleware from "./middlewares/handleAppErrorMiddleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(handleAppErrorMiddleware);
 
 app.listen(process.env.PORT || 3000, () =>
   console.log("server listening on port " + (process.env.PORT || 3000))
