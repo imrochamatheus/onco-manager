11a160729903fcb0431cdbe1e649443701831b75
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-12 08:58:40 -0300

    feat: implementada classe AppErrors

---
+++ src/errors/AppError.ts
@@ -0,0 +1,10 @@
+export default class AppError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number = 400) {
+    super();
+
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
