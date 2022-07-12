12c6d14865551097a0e22b66b8095c432e8fa4ed
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-12 09:18:45 -0300

    feat: implementado handle de erros global

---
+++ src/middlewares/handleAppErrorMiddleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import AppError from "../errors/AppError";
+
+const handleAppErrorMiddleware = (
+  error: Error,
+  req: Request,
+  res: Response,
+  _: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  res.status(500).json({ message: error.message });
+};
+
+export default handleAppErrorMiddleware;
