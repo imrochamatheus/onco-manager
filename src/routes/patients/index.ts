1bf83372ff4b2e7e913cfa0099f7888569909bfe
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-14 19:59:59 -0300

    feat: cria rotas dos pacientes

---
+++ src/routes/patients/index.ts
@@ -0,0 +1,30 @@
+import { Request, Response, Router } from "express";
+
+const patientsRouter = Router();
+
+//create patient
+patientsRouter.post("/", (req: Request, res: Response) => {
+  // createPatientController.handle(req, res)
+});
+
+//get all patients
+patientsRouter.get("/", (req: Request, res: Response) => {
+  // getPatientByIdController.handle(req, res)
+});
+
+//get patient by id
+patientsRouter.get("/:id", (req: Request, res: Response) => {
+  // getPatientByIdController.handle(req, res)
+});
+
+//edit patient by id
+patientsRouter.patch("/:id", (req: Request, res: Response) => {
+  // patchPatientByIdController.handle(req, res)
+});
+
+//delete patient by id
+patientsRouter.delete("/:id", (req: Request, res: Response) => {
+  // deletePatientByIdController.handle(req, res)
+});
+
+export { patientsRouter };
