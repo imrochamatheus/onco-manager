c18213233aeac0e996fdc94b705d1de7c6e42a25
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-14 20:05:04 -0300

    feat: adiciona as rotas patients no arquivo geral das rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { usersRouter } from "./users";
 import { Router } from "express";
+import { patientsRouter } from "./patients";
 
 const router = Router();
 
 router.use("/users", usersRouter);
+router.use("/patients", patientsRouter);
 
 export default router;
