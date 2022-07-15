90d55d6a0ba617e0d94e99daa974f20689bf8d36
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 14:25:44 -0300

    adiciona o controller de deleção ao controller geral do patient

--- src/controllers/patients/index.ts
+++ src/controllers/patients/index.ts
@@ -1,10 +1,12 @@
 import {
   createPatientSvc,
+  deletePatientByIdSvc,
   getAllPatientsSvc,
   getPatientByIdSvc,
 } from "../../services/patients";
 
 import { CreatePatientCtrl } from "./createPatient.ctrl";
+import { DeletePatientByIdCtrl } from "./deletePatientById.ctrl";
 import { GetAllPatientsCtrl } from "./getAllPatients.ctrl";
 import { GetPatientByIdCtrl } from "./getPatientById.ctrl";
 
@@ -12,14 +14,14 @@ const createPatientCtrl = new CreatePatientCtrl(createPatientSvc);
 const getAllPatientsCtrl = new GetAllPatientsCtrl(getAllPatientsSvc);
 const getPatientByIdCtrl = new GetPatientByIdCtrl(getPatientByIdSvc);
 // const patchPatientByIdCtrl = new PatchPatientByIdCtrl(createPatientByIdSvc);
-// const deletePatientByIdCtrl = new DeletePatientByIdCtrl(deletePatientByIdSvc);
+const deletePatientByIdCtrl = new DeletePatientByIdCtrl(deletePatientByIdSvc);
 
 export {
   createPatientCtrl,
   getAllPatientsCtrl,
   getPatientByIdCtrl,
   // patchPatientByIdCtrl,
-  // deletePatientByIdCtrl,
+  deletePatientByIdCtrl,
 };
 
 // export class PatientCtrls {
