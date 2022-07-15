181c6c4731ba994aa3b3e9224fb4df35005e9034
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 13:25:11 -0300

    feat: adiciona os services de getAllPatients e getPatientsById ao service geral de patients

--- src/services/patients/index.ts
+++ src/services/patients/index.ts
@@ -1,8 +1,18 @@
 import { PatientRepository } from "../../repositories/implementations/IPatientRepository";
 import { CreatePatientSvc } from "./createPatient.svc";
+import { GetAllPatientsSvc } from "./getAllPatients.svc";
+import { GetPatientByIdSvc } from "./getPatientById.svc";
 
 const patientRepository = new PatientRepository();
 
 const createPatientSvc = new CreatePatientSvc(patientRepository);
+const getAllPatientsSvc = new GetAllPatientsSvc(patientRepository);
+const getPatientByIdSvc = new GetPatientByIdSvc(patientRepository);
 
-export { createPatientSvc };
+export { createPatientSvc, getAllPatientsSvc, getPatientByIdSvc };
+
+// export class PatientSvcs {
+//   static patientRepository = new PatientRepository();
+
+//   static create = new CreatePatientSvc(this.patientRepository);
+// }
