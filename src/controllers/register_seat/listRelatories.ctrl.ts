89b00bc37096a5620d4e4c224f6335e9d8b99df8
Gisela Mariano <gisela-mariano@gotmail.com>
2022-07-15 15:35:36 -0300

    feat: Listar relatórios (do registro do acento) por data

---
+++ src/controllers/register_seat/listRelatories.ctrl.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { ListRelatoriesService } from "../../services/register_seat/listRelatories.svc";
+
+
+export class ListRelatoriesController {
+  constructor(private listRelatoriesService: ListRelatoriesService){}
+
+  async handle(req: Request, res: Response): Promise<Response> {
+
+    const filter_date = req.params.date;
+
+    const relatories = await this.listRelatoriesService.execute({filter_date});
+
+    return res.status(200).json(relatories);
+  }
+}
