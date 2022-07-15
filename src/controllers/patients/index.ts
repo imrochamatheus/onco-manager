2b8b021d8d7759e1e7501faff583a47ed3ec81f3
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 12:20:48 -0300

    feat: adiciona o controller de criar de patients no arquivo geral de controllers de patients

--- src/controllers/patients/index.ts
+++ src/controllers/patients/index.ts
@@ -1,13 +1,16 @@
-// const createPatientCtrl = new CreatePatientCtrl(createPatientSvc);
+import { createPatientSvc } from "../../services/patients";
+import { CreatePatientCtrl } from "./createPatient.ctrl";
+
+const createPatientCtrl = new CreatePatientCtrl(createPatientSvc);
 // const getAllPatientsCtrl = new GetAllPatientsCtrl(getAllPatientsSvc);
 // const getPatientByIdCtrl = new GetPatientByIdCtrl(getPatientByIdSvc);
 // const patchPatientByIdCtrl = new PatchPatientByIdCtrl(createPatientByIdSvc);
 // const deletePatientByIdCtrl = new DeletePatientByIdCtrl(deletePatientByIdSvc);
 
-// export {
-//   createPatientCtrl,
-//   getAllPatientsCtrl,
-//   getPatientByIdCtrl,
-//   patchPatientByIdCtrl,
-//   deletePatientByIdCtrl,
-// };
+export {
+  createPatientCtrl,
+  // getAllPatientsCtrl,
+  // getPatientByIdCtrl,
+  // patchPatientByIdCtrl,
+  // deletePatientByIdCtrl,
+};
