d17ff0c3d5018fdaaae7611cba5ecb479ad7ed3b
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 13:22:05 -0300

    feat: adiciona a rota para pegar todos os patients e um patient pelo id

--- src/routes/patients/index.ts
+++ src/routes/patients/index.ts
@@ -1,21 +1,27 @@
 import { Request, Response, Router } from "express";
-import { createPatientCtrl } from "../../controllers/patients";
+// import { PatientCtrls } from "../../controllers/patients";
+import {
+  createPatientCtrl,
+  getAllPatientsCtrl,
+  getPatientByIdCtrl,
+} from "../../controllers/patients";
 
 const patientsRouter = Router();
 
 //create patient
 patientsRouter.post("/", (req: Request, res: Response) => {
   createPatientCtrl.handle(req, res);
+  // PatientCtrls.create.handle(req, res);
 });
 
 //get all patients
 patientsRouter.get("/", (req: Request, res: Response) => {
-  // getPatientByIdController.handle(req, res)
+  getAllPatientsCtrl.handle(req, res);
 });
 
 //get patient by id
 patientsRouter.get("/:id", (req: Request, res: Response) => {
-  // getPatientByIdController.handle(req, res)
+  getPatientByIdCtrl.handle(req, res);
 });
 
 //edit patient by id
