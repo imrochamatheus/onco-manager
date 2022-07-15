92c035f966228333cf5241d064d6cea492f0bb2b
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-15 20:52:06 -0300

    feat: Adiciona validação de medical records number

--- src/schemas/patient/index.ts
+++ src/schemas/patient/index.ts
@@ -5,9 +5,11 @@ export const patientCreateSchema = yup.object().shape({
   medical_records_number: yup
     .string()
     .required()
+    .matches(/[^A-z]/)
     .max(255, "Must be a maximum of 255 characters"),
   contact: yup
     .string()
+    .matches(/[^A-z]/)
     .required()
     .max(255, "Must be a maximum of 255 characters"),
 });
@@ -16,6 +18,10 @@ export const patientPatchSchema = yup.object().shape({
   name: yup.string(),
   medical_records_number: yup
     .string()
+    .matches(/[^A-z]/)
     .max(255, "Must be a maximum of 255 characters"),
-  contact: yup.string().max(255, "Must be a maximum of 255 characters"),
+  contact: yup
+    .string()
+    .matches(/[^A-z]/)
+    .max(255, "Must be a maximum of 255 characters"),
 });
