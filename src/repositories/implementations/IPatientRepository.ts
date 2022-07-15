4a5f0f8363b6013b847b601f0782519d3675d535
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 11:07:32 -0300

    feat: cria o repositório de patients

---
+++ src/repositories/implementations/IPatientRepository.ts
@@ -0,0 +1,65 @@
+import { Patient, PrismaClient } from "@prisma/client";
+import {
+  IPatientCreateReq,
+  IPatient,
+  IPatientByIdReq,
+} from "../../interfaces/patient.interface";
+import { IPatientRepository } from "./patients";
+
+class PatientRepository implements IPatientRepository {
+  prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  public async createPatient({
+    name,
+    medical_records_number,
+    contact,
+  }: IPatientCreateReq): Promise<IPatient> {
+    const newPatient = await this.prisma.patient.create({
+      data: {
+        name,
+        medical_records_number,
+        contact,
+      },
+    });
+
+    return newPatient;
+  }
+
+  public async listAllPatients(): Promise<IPatient[]> {
+    const allPatients = await this.prisma.patient.findMany();
+    return allPatients;
+  }
+
+  public async listPatientById({
+    patient_id,
+  }: IPatientByIdReq): Promise<IPatient | null> {
+    const patient = await this.prisma.patient.findUnique({
+      where: { id: patient_id },
+    });
+
+    return patient;
+  }
+
+  public async patchPatientById(
+    { patient_id }: IPatientByIdReq,
+    data: IPatientCreateReq
+  ): Promise<void> {
+    // await this.prisma.patient.update({
+    //   where: { id: patient_id },
+    //   data: { ...data },
+    // });
+    console.log(patient_id, data);
+  }
+
+  public async deletePatientById({
+    patient_id,
+  }: IPatientByIdReq): Promise<void> {
+    await this.prisma.patient.delete({ where: { id: patient_id } });
+  }
+}
+
+export { PatientRepository };
