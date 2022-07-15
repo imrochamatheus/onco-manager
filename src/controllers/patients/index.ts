e8bd4e9fe7ada0da925fe88ed55940c9edb2d41a
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 15:51:19 -0300

    feat: adiciona service e cotroller do patchPatientById aos respectivos arquivos gerais

--- src/controllers/patients/index.ts
+++ src/controllers/patients/index.ts
@@ -3,24 +3,26 @@ import {
   deletePatientByIdSvc,
   getAllPatientsSvc,
   getPatientByIdSvc,
+  patchPatientByIdSvc,
 } from "../../services/patients";
 
 import { CreatePatientCtrl } from "./createPatient.ctrl";
 import { DeletePatientByIdCtrl } from "./deletePatientById.ctrl";
 import { GetAllPatientsCtrl } from "./getAllPatients.ctrl";
 import { GetPatientByIdCtrl } from "./getPatientById.ctrl";
+import { PatchPatientByIdCtrl } from "./patchPatientById.ctrl";
 
 const createPatientCtrl = new CreatePatientCtrl(createPatientSvc);
 const getAllPatientsCtrl = new GetAllPatientsCtrl(getAllPatientsSvc);
 const getPatientByIdCtrl = new GetPatientByIdCtrl(getPatientByIdSvc);
-// const patchPatientByIdCtrl = new PatchPatientByIdCtrl(createPatientByIdSvc);
+const patchPatientByIdCtrl = new PatchPatientByIdCtrl(patchPatientByIdSvc);
 const deletePatientByIdCtrl = new DeletePatientByIdCtrl(deletePatientByIdSvc);
 
 export {
   createPatientCtrl,
   getAllPatientsCtrl,
   getPatientByIdCtrl,
-  // patchPatientByIdCtrl,
+  patchPatientByIdCtrl,
   deletePatientByIdCtrl,
 };
 
--- src/services/patients/index.ts
+++ src/services/patients/index.ts
@@ -1,21 +1,24 @@
 import { PatientRepository } from "../../repositories/implementations/patients";
+
 import { CreatePatientSvc } from "./createPatient.svc";
 import { DeletePatientByIdSvc } from "./deletePatientById.svc";
 import { GetAllPatientsSvc } from "./getAllPatients.svc";
 import { GetPatientByIdSvc } from "./getPatientById.svc";
+import { PatchPatientByIdSvc } from "./patchPatientById.svc";
 
 const patientRepository = new PatientRepository();
 
 const createPatientSvc = new CreatePatientSvc(patientRepository);
 const getAllPatientsSvc = new GetAllPatientsSvc(patientRepository);
 const getPatientByIdSvc = new GetPatientByIdSvc(patientRepository);
-//patch
+const patchPatientByIdSvc = new PatchPatientByIdSvc(patientRepository);
 const deletePatientByIdSvc = new DeletePatientByIdSvc(patientRepository);
 
 export {
   createPatientSvc,
   getAllPatientsSvc,
   getPatientByIdSvc,
+  patchPatientByIdSvc,
   deletePatientByIdSvc,
 };
 
