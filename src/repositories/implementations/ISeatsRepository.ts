0979a94613953164ad80c90db165ad326ec5a5a1
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-15 13:55:15 -0300

    fix: Altera retorno do service de deleção de seat

--- src/repositories/implementations/ISeatsRepository.ts
+++ src/repositories/implementations/ISeatsRepository.ts
@@ -1,16 +1,9 @@
 import { ISeatCreate, ISeat, ISeatId } from "../../interfaces/seats.interfaces";
 
 interface ISeatsRepository {
-  createSeat({ seat_number }: ISeatCreate): Promise<ISeat>;
-<<<<<<< feat-crud-seat
-
   listAllSeats(): Promise<ISeat[]>;
-
   deleteSeat({ seat_id }: ISeatId): Promise<void>;
-=======
-  deleteSeat({ seat_id }: ISeatId): Promise<Boolean>;
-  listAllSeats(): Promise<ISeat[]>;
->>>>>>> development
+  createSeat({ seat_number }: ISeatCreate): Promise<ISeat>;
 }
 
 export { ISeatsRepository };
