57ba56c5e38787026561b6e53206fcbe004d4895
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 10:05:16 -0300

    feat: adiciona interfaces para patients no arquivo patient.interface.ts

---
+++ src/interfaces/patient.interface.ts
@@ -0,0 +1,13 @@
+export interface IPatientCreateReq {
+  name: string;
+  medical_records_number: number;
+  contact: string;
+}
+
+export interface IPatient extends IPatientCreateReq {
+  id: string;
+}
+
+export interface IPatientByIdReq {
+  patient_id: string;
+}
