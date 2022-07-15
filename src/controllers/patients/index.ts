bbc4f71530491ec84ccd0ab8c679732df61111fd
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 13:24:23 -0300

    feat: adiciona os controllers de getAllPatients e getPatientsById ao controller geral de patients

--- src/controllers/patients/index.ts
+++ src/controllers/patients/index.ts
@@ -1,16 +1,27 @@
-import { createPatientSvc } from "../../services/patients";
+import {
+  createPatientSvc,
+  getAllPatientsSvc,
+  getPatientByIdSvc,
+} from "../../services/patients";
+
 import { CreatePatientCtrl } from "./createPatient.ctrl";
+import { GetAllPatientsCtrl } from "./getAllPatients.ctrl";
+import { GetPatientByIdCtrl } from "./getPatientById.ctrl";
 
 const createPatientCtrl = new CreatePatientCtrl(createPatientSvc);
-// const getAllPatientsCtrl = new GetAllPatientsCtrl(getAllPatientsSvc);
-// const getPatientByIdCtrl = new GetPatientByIdCtrl(getPatientByIdSvc);
+const getAllPatientsCtrl = new GetAllPatientsCtrl(getAllPatientsSvc);
+const getPatientByIdCtrl = new GetPatientByIdCtrl(getPatientByIdSvc);
 // const patchPatientByIdCtrl = new PatchPatientByIdCtrl(createPatientByIdSvc);
 // const deletePatientByIdCtrl = new DeletePatientByIdCtrl(deletePatientByIdSvc);
 
 export {
   createPatientCtrl,
-  // getAllPatientsCtrl,
-  // getPatientByIdCtrl,
+  getAllPatientsCtrl,
+  getPatientByIdCtrl,
   // patchPatientByIdCtrl,
   // deletePatientByIdCtrl,
 };
+
+// export class PatientCtrls {
+//   static create = new CreatePatientCtrl(PatientSvcs.create);
+// }
