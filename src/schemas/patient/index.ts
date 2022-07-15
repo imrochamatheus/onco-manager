0df9887f7095968d59af34a4bc8ed39e7e9d44f5
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 11:19:00 -0300

    feat: cria schema do yup para o patch de patients

--- src/schemas/patient/index.ts
+++ src/schemas/patient/index.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-const patientSchema = yup.object().shape({
+export const patientCreateSchema = yup.object().shape({
   name: yup.string().required(),
   medical_records_number: yup.number().required(),
   contact: yup
@@ -9,4 +9,8 @@ const patientSchema = yup.object().shape({
     .max(255, "Must be a maximum of 255 characters"),
 });
 
-export default patientSchema;
+export const patientPatchSchema = yup.object().shape({
+  name: yup.string(),
+  medical_records_number: yup.number(),
+  contact: yup.string().max(255, "Must be a maximum of 255 characters"),
+});
