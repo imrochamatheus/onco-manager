a458973d4743fa9b4a02b1dce41cbde562efec93
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-15 14:22:21 -0300

    feat: adiciona o service de deleção ao service geral do patient

--- src/services/patients/index.ts
+++ src/services/patients/index.ts
@@ -1,5 +1,6 @@
-import { PatientRepository } from "../../repositories/implementations/IPatientRepository";
+import { PatientRepository } from "../../repositories/implementations/patients";
 import { CreatePatientSvc } from "./createPatient.svc";
+import { DeletePatientByIdSvc } from "./deletePatientById.svc";
 import { GetAllPatientsSvc } from "./getAllPatients.svc";
 import { GetPatientByIdSvc } from "./getPatientById.svc";
 
@@ -8,8 +9,15 @@ const patientRepository = new PatientRepository();
 const createPatientSvc = new CreatePatientSvc(patientRepository);
 const getAllPatientsSvc = new GetAllPatientsSvc(patientRepository);
 const getPatientByIdSvc = new GetPatientByIdSvc(patientRepository);
+//patch
+const deletePatientByIdSvc = new DeletePatientByIdSvc(patientRepository);
 
-export { createPatientSvc, getAllPatientsSvc, getPatientByIdSvc };
+export {
+  createPatientSvc,
+  getAllPatientsSvc,
+  getPatientByIdSvc,
+  deletePatientByIdSvc,
+};
 
 // export class PatientSvcs {
 //   static patientRepository = new PatientRepository();
