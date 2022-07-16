5bd1f5de9b35c92c17746d898c3ad89ab32920b3
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-16 18:03:58 -0300

    feat: Adiciona modo restrito ao schema de protocolos

--- src/schemas/protocol/index.ts
+++ src/schemas/protocol/index.ts
@@ -1,13 +1,27 @@
 import * as yup from "yup";
 
-const protocolSchema = yup.object().shape({
-  name: yup.string().required(),
-  volume: yup.number().required(),
-  infusion: yup.string(),
-  description: yup
-    .string()
-    .required()
-    .max(255, "Must be a maximum of 255 characters"),
-});
+const protocolSchema = yup
+  .object()
+  .shape({
+    name: yup.string().required(),
+    volume: yup.number().required(),
+    infusion_time: yup.string(),
+    description: yup
+      .string()
+      .required()
+      .max(255, "Must be a maximum of 255 characters"),
+  })
+  .strict()
+  .noUnknown(true, "Enter only valid keys");
 
-export default protocolSchema;
+const protocolPatchSchema = yup
+  .object({
+    name: yup.string(),
+    volume: yup.number(),
+    infusion_time: yup.string(),
+    description: yup.string().max(255, "Must be a maximum of 255 characters"),
+  })
+  .strict()
+  .noUnknown(true, "Enter only valid keys");
+
+export { protocolSchema, protocolPatchSchema };
