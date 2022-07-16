235f02eea6c1642bccc7f0ef60a9b8411a39117e
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-16 18:04:35 -0300

    feat: Adiciona rotas para busca, atualização e deleção de protocolos

--- src/routes/protocols/index.ts
+++ src/routes/protocols/index.ts
@@ -1,16 +1,52 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router, NextFunction } from "express";
 import {
   createProtocolController,
+  deleteProtocolController,
+  getProtocolById,
   listProtocolsController,
+  patchProtocolByIdController,
 } from "../../controllers/protocols";
+import checkIfProtocolExists from "../../middlewares/checkIfProtocolExists.mdw";
+import schemaValidation from "../../middlewares/schemaValidation.mdw";
+import { protocolSchema, protocolPatchSchema } from "../../schemas/protocol";
 
 const protocolsRouter = Router();
 
-protocolsRouter.post("/", (req: Request, res: Response) => {
-  createProtocolController.handle(req, res);
-});
 protocolsRouter.get("/", (req: Request, res: Response) => {
   listProtocolsController.handle(req, res);
 });
+
+protocolsRouter.get(
+  "/:id",
+  checkIfProtocolExists,
+  (req: Request, res: Response, next: NextFunction) => {
+    getProtocolById.handle(req, res, next);
+  }
+);
+
+protocolsRouter.patch(
+  "/:id",
+  checkIfProtocolExists,
+  schemaValidation(protocolPatchSchema),
+  (req: Request, res: Response, next: NextFunction) => {
+    patchProtocolByIdController.handle(req, res, next);
+  }
+);
+
+protocolsRouter.delete(
+  "/:id",
+  checkIfProtocolExists,
+  (req: Request, res: Response, next: NextFunction) => {
+    deleteProtocolController.handle(req, res, next);
+  }
+);
+
+protocolsRouter.post(
+  "/",
+  schemaValidation(protocolSchema),
+  (req: Request, res: Response, next: NextFunction) => {
+    createProtocolController.handle(req, res, next);
+  }
+);
 
 export { protocolsRouter };
