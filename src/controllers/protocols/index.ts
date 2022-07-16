2a7d85270038f00769dc3bc48919995e9c145d1b
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-16 18:07:21 -0300

    feat: Adiciona controllers de busca, atualização e deleção de protocolos

--- src/controllers/protocols/index.ts
+++ src/controllers/protocols/index.ts
@@ -1,15 +1,34 @@
 import {
   createProtocolService,
+  deleteProtocolService,
   listAllProtocolsService,
+  readOneProtocolService,
+  updateProtocolService,
 } from "../../services/protocols";
-import { CreateProtocolController } from "./createProtocol.ctrl";
+import { GetProtocolById } from "./getProtocolById.ctrl";
 import { ListProtocolsController } from "./listProtocols.ctrl";
+import { CreateProtocolController } from "./createProtocol.ctrl";
+import { DeleteProtocolController } from "./deleteProtocol.ctrl";
+import { PatchProtocolByIdController } from "./patchProtocolById.ctrl";
 
+const getProtocolById = new GetProtocolById(readOneProtocolService);
+const deleteProtocolController = new DeleteProtocolController(
+  deleteProtocolService
+);
 const createProtocolController = new CreateProtocolController(
   createProtocolService
 );
 const listProtocolsController = new ListProtocolsController(
   listAllProtocolsService
 );
+const patchProtocolByIdController = new PatchProtocolByIdController(
+  updateProtocolService
+);
 
-export { createProtocolController, listProtocolsController };
+export {
+  getProtocolById,
+  listProtocolsController,
+  createProtocolController,
+  deleteProtocolController,
+  patchProtocolByIdController,
+};
