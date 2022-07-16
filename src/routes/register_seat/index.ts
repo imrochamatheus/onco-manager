0376f7ce649c91ce01b6a55aa4a6a4642dc7013e
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-16 19:25:13 -0300

    feat: Adiciona rota inicial

--- src/routes/register_seat/index.ts
+++ src/routes/register_seat/index.ts
@@ -1,15 +1,19 @@
 import { Request, Response, Router } from "express";
-import { createRegisterSeatController, listRelatoriesController } from "../../controllers/register_seat";
-
+import {
+  createRegisterSeatController,
+  listRelatoriesController,
+} from "../../controllers/register_seat";
 
 const registerSeatRouter = Router();
 
+registerSeatRouter.post("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "hello" });
+});
 registerSeatRouter.post("/register_seat", (req: Request, res: Response) => {
   createRegisterSeatController.handle(req, res);
 });
 registerSeatRouter.get("/relatories/:date", (req: Request, res: Response) => {
   listRelatoriesController.handle(req, res);
 });
-
 
 export { registerSeatRouter };
