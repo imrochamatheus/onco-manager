f3b57e90d399eff223642dfeb90e6c10a4705457
imrochamatheus <im.rochamatheus@gmail.com>
2022-07-17 01:26:06 -0300

    refactor: Adiciona regra para utilização apenas de caracteres alfabéticos no nome do profissional

--- src/schemas/professionals/index.ts
+++ src/schemas/professionals/index.ts
@@ -6,11 +6,13 @@ const professionalSchema = yup
     full_name: yup
       .string()
       .required()
+      .matches(/^([a-zA-Zà-úÀ-Ú]\s)+$/, "Must contain only letters")
       .max(255, "Must be a maximum of 255 characters"),
     email: yup.string().email().required(),
     cartao_nacional_saude: yup
       .string()
       .required()
+      .matches(/[^A-z]/, "Must contain only numbers")
       .max(255, "Must be a maximum of 255 characters"),
     password: yup
       .string()
