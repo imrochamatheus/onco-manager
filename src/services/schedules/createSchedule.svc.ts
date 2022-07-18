2498207810a34e08448d773751771c9138da9c70
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-18 15:05:11 -0300

    comment: cria passo a passo para verificar agendamento

--- src/services/schedules/createSchedule.svc.ts
+++ src/services/schedules/createSchedule.svc.ts
@@ -13,10 +13,20 @@ class CreateScheduleSvc {
     cicle_number,
     date,
   }: IScheduleCreate): Promise<ISchedule> {
+    const schedules = await this.scheduleRepository.getAllSchedules();
+
+    //verificar se tem cadeiras disponiveis no horário
+
     //verificar disponibilidade de data
-    //1 - listar todas os agendamentos do dia
-    //2 - horario marcado + tempo de protocolo + 15 min
-    ///2.1: if
+    //1 - listar todas os agendamentos do horario e dia
+    //2 - verficar se tem cadeiras livres no horario
+    //3 - listar todas as cadeiras
+    //4 - verficar se o numero de agendamentos é menor ao numero de cadeiras.
+    ///4.1 Se for igual, não há cadeiras disponíveis para aquele horário.
+    ///4.2 se for menor, agenda normalmente
+
+    //4.1.1 - pegar o infusion_time do protocolo a ser agendado e acrescentar o tempo de limpeza (15 min)
+    //4.1.2 -
 
     return await this.scheduleRepository.createSchedule({
       id_patient,
