1d33a2f14ea4d76d342e3c0b63c05fcd90acfe19
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-18 13:48:08 -0300

    feat: adiciona verificação de autorização nas rotas de schedules

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,27 @@
 import { professionalsRouter } from "./professionals";
-import { registerSeatRouter } from "./register_seat";
 import { protocolsRouter } from "./protocols";
 import { patientsRouter } from "./patients";
 import { usersRouter } from "./users";
 import { seatsRouter } from "./seats";
 import { sessionsRouter } from "./sessions";
 import { Router } from "express";
+import { registerSeatRouter } from "./register_seat";
+import { schedulesRouter } from "./schedules";
+import authorizarionMiddleware from "../middlewares/authorization.mdw";
 
 const router = Router();
 
 router.use("/users", usersRouter);
 router.use("/seats", seatsRouter);
-router.use("/", registerSeatRouter);
 router.use("/login", sessionsRouter);
 router.use("/patients", patientsRouter);
 router.use("/protocols", protocolsRouter);
 router.use("/professionals", professionalsRouter);
+router.use(
+  "/schedules",
+  authorizarionMiddleware(["master", "staff"]),
+  schedulesRouter
+);
+router.use("/", registerSeatRouter);
 
 export default router;
