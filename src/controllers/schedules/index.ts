811a3b291819fa64ae0ccfd9d872c40b8643fb16
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-18 21:23:31 -0300

    refactor: renomeia métodos

--- src/controllers/schedules/index.ts
+++ src/controllers/schedules/index.ts
@@ -1,27 +1,33 @@
 import {
   createScheduleSvc,
-  deleteScheduleById,
+  deleteScheduleByIdSvc,
   getAllSchedulesSvc,
+  getScheduleByDateSvc,
   getScheduleByIdSvc,
-  patchScheduleById,
+  patchScheduleByIdSvc,
 } from "../../services/schedules";
 
 import { CreateScheduleCtrl } from "./createSchedule.ctrl";
 import { DeleteScheduleByIdCtrl } from "./deleteScheduleById.ctrl";
 import { GetAllSchedulesCtrl } from "./getAllSchedules.ctrl";
+import { GetScheduleByDateCtrl } from "./getScheduleByDate.ctrl";
 import { GetScheduleByIdCtrl } from "./getScheduleById.ctrl";
 import { PatchScheduleByIdCtrl } from "./patchScheduleById.ctrl";
 
 const createScheduleCtrl = new CreateScheduleCtrl(createScheduleSvc);
 const getAllSchedulesCtrl = new GetAllSchedulesCtrl(getAllSchedulesSvc);
 const getScheduleByIdCtrl = new GetScheduleByIdCtrl(getScheduleByIdSvc);
-const patchScheduleByIdCtrl = new PatchScheduleByIdCtrl(patchScheduleById);
-const deleteScheduleByIdCtrl = new DeleteScheduleByIdCtrl(deleteScheduleById);
+const getScheduleByDateCtrl = new GetScheduleByDateCtrl(getScheduleByDateSvc);
+const patchScheduleByIdCtrl = new PatchScheduleByIdCtrl(patchScheduleByIdSvc);
+const deleteScheduleByIdCtrl = new DeleteScheduleByIdCtrl(
+  deleteScheduleByIdSvc
+);
 
 export {
   createScheduleCtrl,
   getAllSchedulesCtrl,
   getScheduleByIdCtrl,
+  getScheduleByDateCtrl,
   patchScheduleByIdCtrl,
   deleteScheduleByIdCtrl,
 };
