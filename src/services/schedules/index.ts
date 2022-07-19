e4308c627288c4b7030d2e9f50aa22f1fdfd87fd
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-18 21:24:52 -0300

    refactor: renomeia métodos

--- src/services/schedules/index.ts
+++ src/services/schedules/index.ts
@@ -3,6 +3,7 @@ import { ScheduleRepository } from "../../repositories/implementations/schedules
 import { CreateScheduleSvc } from "./createSchedule.svc";
 import { DeleteScheduleByIdSvc } from "./deleteSchedule.svc";
 import { GetAllSchedulesSvc } from "./getAllSchedules.svc";
+import { GetScheduleByDateSvc } from "./getScheduleByDate.svc";
 import { GetScheduleByIdSvc } from "./getScheduleById.svc";
 import { PatchScheduleByIdSvc } from "./patchScheduleById.svc";
 
@@ -11,13 +12,15 @@ const scheduleRepository = new ScheduleRepository();
 const createScheduleSvc = new CreateScheduleSvc(scheduleRepository);
 const getAllSchedulesSvc = new GetAllSchedulesSvc(scheduleRepository);
 const getScheduleByIdSvc = new GetScheduleByIdSvc(scheduleRepository);
-const patchScheduleById = new PatchScheduleByIdSvc(scheduleRepository);
-const deleteScheduleById = new DeleteScheduleByIdSvc(scheduleRepository);
+const getScheduleByDateSvc = new GetScheduleByDateSvc(scheduleRepository);
+const patchScheduleByIdSvc = new PatchScheduleByIdSvc(scheduleRepository);
+const deleteScheduleByIdSvc = new DeleteScheduleByIdSvc(scheduleRepository);
 
 export {
   createScheduleSvc,
   getAllSchedulesSvc,
   getScheduleByIdSvc,
-  patchScheduleById,
-  deleteScheduleById,
+  getScheduleByDateSvc,
+  patchScheduleByIdSvc,
+  deleteScheduleByIdSvc,
 };
