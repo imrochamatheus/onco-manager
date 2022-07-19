62fdab8584e6a0043e200159d1d8f5b8cf1cb852
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-18 21:25:54 -0300

    feat: implementa lógica que verifica a disponibilidade de um agendamento pela data

--- src/services/schedules/createSchedule.svc.ts
+++ src/services/schedules/createSchedule.svc.ts
@@ -4,6 +4,9 @@ import {
 } from "../../interfaces/schedules.interface";
 import { IScheduleRepository } from "../../repositories/implementations/schedules/IScheduleRepository";
 
+import moment from "moment";
+import { SeatsRepository } from "../../repositories/implementations/seats";
+import AppError from "../../errors/AppError";
 class CreateScheduleSvc {
   constructor(private scheduleRepository: IScheduleRepository) {}
 
@@ -13,7 +16,39 @@ class CreateScheduleSvc {
     cicle_number,
     date,
   }: IScheduleCreate): Promise<ISchedule> {
-    const schedules = await this.scheduleRepository.getAllSchedules();
+    const schedule_date = new Date(date).toISOString().substring(0, 10);
+
+    const seatRepo = new SeatsRepository();
+    const allSeats = await seatRepo.listAllSeats();
+
+    const schedulesOnDate = await this.scheduleRepository.getScheduleByDate({
+      schedule_date,
+    });
+
+    const newSchedules = schedulesOnDate?.map((item) => {
+      const minimun_time = parseInt(item.protocol!.infusion_time) + 15;
+
+      item.final_date = moment(new Date(item.date))
+        .add(minimun_time, "m")
+        .add(3, "h");
+      return item;
+    });
+
+    const currentDate = moment(new Date(date)).add(3, "h");
+    let counter = 0;
+
+    newSchedules?.forEach((schedule) => {
+      const scheduleFinalDate = schedule.final_date;
+
+      if (currentDate.diff(scheduleFinalDate) <= 0) {
+        counter++;
+      }
+    });
+
+    if (allSeats.length <= counter) {
+      throw new AppError("No seats available", 400);
+    }
+    //comparar os horários das schedules e verificar se tem alguma em conflito com a atual
 
     //verificar se tem cadeiras disponiveis no horário
 
